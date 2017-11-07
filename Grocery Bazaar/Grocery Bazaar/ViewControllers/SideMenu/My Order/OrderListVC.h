c94018ae2ad1f695a3e2876da6325304034fa5ab
NetprophetsMAC <shubham.gupta@netprophetsglobal.com>
2017-11-07 16:38:35 +0530

    OrderListVC Added

---
+++ Grocery Bazaar/Grocery Bazaar/ViewControllers/SideMenu/My Order/OrderListVC.h
@@ -0,0 +1,13 @@
+//
+//  OrderListVC.h
+//  Grocery Bazaar
+//
+//  Created by NetprophetsMAC on 11/7/17.
+//  Copyright © 2017 Netprophets. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OrderListVC : UIViewController
+
+@end
